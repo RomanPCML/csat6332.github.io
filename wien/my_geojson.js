85472b5d107e6c27a51054e44cb1992c3e1f6868
Schmidt <csat6332@uibk.ac.at>
2017-05-03 18:25:03 +0200

    Signed-off-by: Schmidt <csat6332@uibk.ac.at>

---
+++ wien/my_geojson.js
@@ -0,0 +1,32 @@
+window.my_geojson = {
+	"type": "FeatureCollection",
+	"features":[
+	{
+		"type": "Feature",
+		"geometry": 
+		{
+			"type": "Point",
+			"coordinates": [16.316,48.227]
+		},
+		"properties": {
+			"color": "black"
+		}
+		
+	},
+	{
+		"type": "Feature",
+		"geometry": 
+		{
+			"type": "Point",
+			"coordinates": [16.320,48.240]
+		},
+			"properties": {
+			"color": "red"
+		}
+		
+	}
+	
+	]
+	
+
+};
